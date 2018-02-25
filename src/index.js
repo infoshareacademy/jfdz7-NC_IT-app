84d048a5d569b048b861d587b4180edddb646c40
rafskii <rafal.dombrowski@gmail.com>
2018-02-25 12:15:41 +0100

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
