73484684dd4102c013ac6de27604e1c9b98640dc
Rafał Dombrowski <rafal.dombrowski@gmail.com>
2018-03-03 14:42:41 +0100

    smallest price display

--- src/components/listOfProducts/ListOfProductsTest.js
+++ src/components/listOfProducts/ListOfProductsTest.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 
 import products from '../../data/products.json'
+import shops from '../../data/shops.json'
 
 import SingleProductToList from "./SingleProductToList";
 
 class ListOfProductsTest extends Component {
     state = {
-        products
+        products,
+        shops
     };
     render() {
         return (
             <React.Fragment>
                 <ul>
-                <SingleProductToList products={this.state.products}/>
+                <SingleProductToList products={this.state.products} shops={this.state.shops}/>
                 </ul>
             </React.Fragment>
         );
--- src/components/listOfProducts/SingleProductToList.js
+++ src/components/listOfProducts/SingleProductToList.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 
+
 class SingleProductToList extends Component {
     render() {
-        const {products} = this.props; //component który dostaje listę kontaktów w propsach i wyświetlam w liście
+        const {products} = this.props; //component który dostaje listę produktów w propsach i wyświetlam w liście
         return (
             <li>
                 {products.map((product, i) => {
                     return (
                         <li key={product.id}>
+                            <div>
+                            <strong>Kategoria: {product.category}</strong><br/>
                             <strong>{product.name}</strong>
+                                <ul>
+                                    <li>
+                                        Najniższa cena:
+                                    {Math.min.apply(Math,product.availabity.map((product) => {
+                                        return (product.price)
+                                }))}
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                     )
                 })}
