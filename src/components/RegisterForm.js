02f95a0cd8adb3c4fbd90a05f80d81aadbda0457
Marcin Płużyński <marcinp2012@gmail.com>
2018-03-10 19:08:34 +0100

    RegisterForm file added

---
+++ src/components/RegisterForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class RegisterForm extends React.Component {
+    render() {
+        return (
+          <h2>Register Form</h2>
+        );
+    }
+}
+
+export default RegisterForm;
