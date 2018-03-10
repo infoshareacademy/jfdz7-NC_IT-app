ef8936b556c3840eeb76d852d8adf89807ee8f1c
Marcin Płużyński <marcinp2012@gmail.com>
2018-03-10 19:25:19 +0100

    signUpUser function added, form rendering added

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,9 +1,55 @@
 import React from 'react';
 
 class RegisterForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [
+                {
+                    id: Date.now().toString(32),
+                    name: 'Jan',
+                    lastName: 'Kowalski',
+                    email: 'jan.kowalski@email.com',
+                    password: ''
+                }
+            ]
+        };
+    }
+
+    signUpUser = ({userName, userLastName, userEmail, userPassword}) => {
+        this.setState({
+            users: this.state.users.concat({
+                id: Date.now().toString(32),
+                name: userName,
+                lastName: userLastName,
+                email: userEmail,
+                password: userPassword
+                }
+            )
+        })
+    };
+
+    passwordVerification = () => {
+
+    };
+
     render() {
         return (
-          <h2>Register Form</h2>
+            <React.Fragment>
+                <h2>Register Form</h2>
+                <form>
+                    Imię
+                    <input/>
+                    Nazwisko
+                    <input/>
+                    E-mail
+                    <input/>
+                    Password
+                    <input/>
+                </form>
+            </React.Fragment>
         );
     }
 }
