85de20fed11f51644d80a9c2590c95b8110e3da0
Dominik Manowski <manowski@protonmail.com>
2018-03-11 12:34:12 +0100

    create SingleProductComponent

---
+++ src/components/singleProductViev/SingleProductView.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class SingleProductView extends Component {
+    render() {
+        return (
+            <h2></h2>
+        )
+    }
+}
+
+export default SingleProductView
