6c82b61611c2a2f6ec101fa7f6e5a0cde23b8829
Dominik Manowski <manowski@protonmail.com>
2018-03-18 11:46:23 +0100

    Add product list view file

---
+++ src/components/productListView/ProductListView.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class ProductListView extends Component {
+    render() {
+        return (
+            <h1>Produkty:</h1>
+        )
+    }
+}
+
+export default ProductListView
