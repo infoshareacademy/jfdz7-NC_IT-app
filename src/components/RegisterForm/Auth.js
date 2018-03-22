ed980027f25ef9674a51e3540041fe1d5115b2a6
Marcin Płużyński <marcinp2012@gmail.com>
2018-03-22 22:16:32 +0100

    Auth added SignInForm and SignUpForm to render

--- src/components/RegisterForm/Auth.js
+++ src/components/RegisterForm/Auth.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {SignUpForm} from './SignUpForm';
+import {SignInForm} from './SignInForm';
 
 class Auth extends React.Component {
 
     render() {
-        return this.props.user === null ? <SignUpForm/> : this.props.children
+        return this.props.user === null ? (
+            <div>
+                <SignUpForm/>
+                <SignInForm/>
+            </div>
+        ) : (this.props.children)
     }
 }
 
